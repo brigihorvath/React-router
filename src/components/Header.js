3b541d5d3ea26448cf065f4407768f1e5f427ddb
Brigi Horvath <brigi.horvath82@gmail.com>
2022-01-13 17:53:01 +0100

    change Links to NavLink in nav

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,16 +1,22 @@
-import { Link } from 'react-router-dom';
+import { NavLink } from 'react-router-dom';
+import classes from './Header.module.css';
 
 const Header = () => {
   return (
-    <header>
-      <h2>Header</h2>
+    <header className={classes.header}>
       <nav>
-        <li>
-          <Link to="/welcome">Welcome</Link>
-        </li>
-        <li>
-          <Link to="/products">Products</Link>
-        </li>
+        <ul>
+          <li>
+            <NavLink activeClassName={classes.active} to="/welcome">
+              Welcome
+            </NavLink>
+          </li>
+          <li>
+            <NavLink activeClassName={classes.active} to="/products">
+              Products
+            </NavLink>
+          </li>
+        </ul>
       </nav>
     </header>
   );
